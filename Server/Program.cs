9bd83e66df52109b37219393e064cfba3df42c08
alexanderkurilenko <alex080397@gmail.com>
2017-05-23 17:38:40 +0300

    first

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +12,75 @@ namespace Server
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Начинаем слушать соединения
+                while (true)
+                {
+                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
+
+                    // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    
+                    int[,] arr;
+                    // Мы дождались клиента, пытающегося с нами соединиться
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    using (MemoryStream fs = new MemoryStream(bytes))
+                    {
+                        arr = (int[,])formatter.Deserialize(fs);
+                        Console.WriteLine("Исходные данные клиента:");
+                        for (int i = 0; i < arr.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < arr.GetLength(1); j++)
+                            {
+                                Console.WriteLine(arr[i, j]);
+                            }
+                        }
+
+                    }
+
+                    string reply = "Спасибо за запрос.Резyльтат: ";
+                   
+                    int[] rslt = Algorithm.KuhnMunkres(arr);
+                    foreach(var it in rslt)
+                    {
+                        reply += it.ToString()+" ";
+                    }
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
+
